d36424bd0b6ad4bb9b9765e68deedb1a47afdee7
Sam Thurman <sam.bbmgmt@gmail.com>
2019-11-05 09:45:03 -0800

    array oriented vertically

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 'use strict'
 
-var timeOfDay = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+var timeOfDay = [
+    '6am',
+    '7am',
+    '8am',
+    '9am',
+    '10am',
+    '11am',
+    '12pm',
+    '1pm',
+    '2pm',
+    '3pm',
+    '4pm',
+    '5pm',
+    '6pm',
+    '7pm',
+];
 var storeNames= [seattle, tokyo, dubai, paris, lima];
 
 function getRandomInt(min, max) {
