e5093e6a4cd7b6e27f696ba4a3cce6abb0062390
Sam Thurman <sam.bbmgmt@gmail.com>
2019-11-06 12:11:52 -0800

    made forms for adding new store, submitted info does nothing rn

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var timeOfDay = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',];
+
 var stores= [seattle, tokyo, dubai, paris, lima];
 var totals = [];
 function getRandomInt(min, max) {
